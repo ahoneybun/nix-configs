765824ce2de94a4774dcbdcd9ede0098492552b5
Aaron  Honeycutt <aaronhoneycutt@protonmail.com>
2022-08-21 23:18:16 +0000

    Update gnome.nix

--- gnome.nix
+++ gnome.nix
@@ -1,5 +1,12 @@
+{ config, pkgs, ... }: 
+
+{
+  
+  # GNOME
   services.xserver = {
     enable = true;
     displayManager.lightdm.enable = true;
     desktopManager.xfce.enable = true;
   };
+
+}
