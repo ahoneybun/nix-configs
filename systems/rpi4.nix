5e1de5ef54f1aa2c51253bc10a54b1029940408f
Aaron  Honeycutt <aaronhoneycutt@protonmail.com>
2022-08-22 23:16:12 +0000

    Upload New File

---
+++ systems/rpi4.nix
@@ -0,0 +1,83 @@
+{ config, pkgs, lib, ... }:
+
+{
+  imports =
+    [
+       "${fetchTarball "https://github.com/NixOS/nixos-hardware/archive/936e4649098d6a5e0762058cb7687be1b2d90550.tar.gz" }/raspberry-pi/4"
+#       ./programs.nix
+    ];
+
+  fileSystems = {
+    "/" = {
+      device = "/dev/disk/by-label/NIXOS_SD";
+      fsType = "ext4";
+      options = [ "noatime" ];
+    };
+  };
+
+  networking = {
+    hostName = "NixOS-rpi4";
+    networkmanager.enable = true;
+  };
+
+  # Set your time zone.
+  time.timeZone = "America/Denver";
+
+  environment.systemPackages = 
+            with pkgs; 
+            [
+                firefox
+                fish
+                git
+                neofetch
+                thunderbird
+                restic
+                wget
+            ]; 
+
+  services.openssh.enable = true;
+
+  # Define user accounts
+    users.extraUsers.aaronh = {
+            description = "Aaron Honeycutt";
+            home = "/home/aaronh";
+            extraGroups = [ "wheel" "networkmanager" "adm"];
+            isNormalUser = true;
+            hashedPassword = "$6$aAcbLtqiqzySifls$jdKMOQjoWITHD/dWNNZVUH/qNc6aoJ7v4zYofi0U7IJSVTbmOfChS3mzaJbp57AodjdPNKPrnrip8Nlh2Qanx.";
+    };
+
+  # Enable Pipewire
+    security.rtkit.enable = true;
+    services.pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+    };
+ 
+  # Turn off PulseAudio
+  hardware.pulseaudio.enable = false;
+
+  # Enable Bluetooth
+  hardware.bluetooth.enable = true;
+
+  # Enable CUPS
+  services.printing.enable = true;
+
+  # Enable GPU acceleration
+  hardware.raspberry-pi."4".fkms-3d.enable = true;
+
+  # Allow Unfree
+  nixpkgs.config.allowUnfree = true;
+
+  # GNOME
+  services.xserver.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+
+  # System 
+  system.stateVersion = "22.11";
+  system.autoUpgrade.enable = true;
+
+
+}
