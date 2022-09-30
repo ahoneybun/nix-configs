2ccb2d90c9af96b1ee4e53c5325608e55d41a43d
Aaron  Honeycutt <aaronhoneycutt@protonmail.com>
2022-09-30 17:42:51 +0000

    Add System76 items

--- systems/oryp6.nix
+++ systems/oryp6.nix
@@ -4,6 +4,9 @@
     # Name your host machine
     networking.hostName = "oryp6"; 
 
+    # System76
+    hardware.system76.enableAll = true;
+
     # NVIDIA
     services.xserver.videoDrivers = [ "nvidia" ];   
     hardware.opengl.enable = true;
