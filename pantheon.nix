c8c4f4980020a3a0c9ad35e28edbf77ae5ac1e0b
Aaron  Honeycutt <aaronhoneycutt@protonmail.com>
2023-01-16 19:42:13 +0000

    Update pantheon.nix

--- pantheon.nix
+++ pantheon.nix
@@ -4,26 +4,11 @@
   # Desktop
   services.xserver = {
     enable = true;
-    displayManager.lightdm.greeters.pantheon.enable = false;
-    displayManager.lightdm.enable = false;
     desktopManager.pantheon.enable = true;
-  };
-
-  # Wingpangel and Switchboard plugins
-  wingpanel-with-indicators.override {
-    indicators = [
-      pkgs.some-special-indicator
-    ];
-  };
-
-  switchboard-with-plugs.override {
-    plugs = [
-      pkgs.some-special-plug
-    ];
   };
   
   # Remove Pantheon packages
-  environment.pantheon.excludePackagess = (with pkgs; [
+  environment.pantheon.excludePackages = (with pkgs; [
   pantheon.appcenter # AppCenter as it can't be used on NixOS
   ]);
 
