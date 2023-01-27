5e09d973772c149ce40c6ec630fa0014ac422ba7
Aaron  Honeycutt <aaronhoneycutt@protonmail.com>
2023-01-27 16:55:11 +0000

    Update linode.nix

--- systems/linode.nix
+++ systems/linode.nix
@@ -50,62 +50,6 @@
   security.acme.acceptTerms = true;
   security.acme.defaults.email = "aaronhoneycutt@proton.me";
 
-  services.nginx = {
-    enable = true;
-    recommendedTlsSettings = true;
-    recommendedOptimisation = true;
-    recommendedGzipSettings = true;
-    recommendedProxySettings = true;
-
-    virtualHosts = {
-      "ahoneybun.net" = {
-      forceSSL = true;
-      enableACME = true;
-      locations."/" = {
-        root = "/var/www/website";
-        };
-      };
-    };
-
-    virtualHosts = {
-      "hydra.ahoneybun.net" = {
-      forceSSL = true;
-      enableACME = true;
-      locations."/" = {
-        proxyPass = "http://127.0.0.1:3000";
-
-        extraConfig = ''
-          etag on;
-          gzip on;
-
-          add_header 'Access-Control-Allow-Origin' '*' always;
-          add_header 'Access-Control-Allow-Methods' 'POST, PUT, DELETE, GET, PATCH, OPTIONS' always;
-          add_header 'Access-Control-Allow-Headers' 'Authorization, Content-Type, Idempotency-Key' always;
-          add_header 'Access-Control-Expose-Headers' 'Link, X-RateLimit-Reset, X-RateLimit-Limit, X-RateLimit-Remaining, X-Request-Id' always;
-
-          if ($request_method = OPTIONS) {
-            return 204;
-          }
-
-          add_header X-XSS-Protection "1; mode=block";
-          add_header X-Permitted-Cross-Domain-Policies none;
-          add_header X-Frame-Options DENY;
-          add_header X-Content-Type-Options nosniff;
-          add_header Referrer-Policy same-origin;
-          add_header X-Download-Options noopen;
-          proxy_http_version 1.1;
-          proxy_set_header Upgrade $http_upgrade;
-          proxy_set_header Connection "upgrade";
-          proxy_set_header Host $host;
-
-          client_max_body_size 16m;
-          # NOTE: increase if users need to upload very big files
-        '';
-        };
-      };
-    };
-  };
-
   # networking.hostName = "nixos"; # Define your hostname.
   # Pick only one of the below networking options.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
