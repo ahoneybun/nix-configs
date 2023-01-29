3b0c643b05df96a0e8494eef07e570ae8503762e
Aaron  Honeycutt <aaronhoneycutt@protonmail.com>
2023-01-29 22:29:43 +0000

    Update gnome.nix

--- desktops/gnome.nix
+++ desktops/gnome.nix
@@ -9,16 +9,20 @@
   };
 
   # Add GNOME packages
-  environment.systemPackages = [
-    pkgs.amberol
-  ];
+  environment.systemPackages = (with pkgs; [
+    amberol
+  ]);
   
+  # GNOME Shell Extensions
+  environment.systemPackages = with pkgs; [ gnomeExtensions.appindicator ];
+
   # Remove GNOME packages
   environment.gnome.excludePackages = (with pkgs; [
-  epiphany # web browser
-  gnome-connections
-  gnome-photos
-  gnome-tour
+    epiphany # web browser
+    gnome-connections
+    gnome-geary
+    gnome-photos
+    gnome-tour
   ]);
 
   # Services
