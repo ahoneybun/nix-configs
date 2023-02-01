895669b5004105960bd6f481f1a8eee3ff005645
Aaron  Honeycutt <aaronhoneycutt@protonmail.com>
2023-02-01 13:59:16 +0000

    Update shell.nix

--- dev/COSMIC/shell.nix
+++ dev/COSMIC/shell.nix
@@ -1,11 +1,28 @@
+# WIP might not work yet
+
 { pkgs ? import <nixpkgs> {} }:
   pkgs.mkShell {
     nativeBuildInputs = with pkgs; [ 
+       appstream-glib
        cargo
+       cmake
+       dbus
+       desktop-file-utils
+       egl-wayland
        glib
+       gtk4
        just
+       llvm
+       libclang
+       libglvnd
+       libinput
+       libpulseaudio
        libxkbcommon
+       mesa
        meson
+       ninja
        pkg-config
+       seatd
+       systemd
        ];
 }
