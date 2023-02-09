18c129b1b948ae4bb3d17a75213229dc6175b32e
Aaron  Honeycutt <aaronhoneycutt@protonmail.com>
2023-02-09 16:24:24 +0000

    Update plasma.nix

--- desktops/plasma.nix
+++ desktops/plasma.nix
@@ -14,15 +14,16 @@
     environment.systemPackages = 
             with pkgs; 
             [
-                libsForQt5.ark
-                libsForQt5.bismuth
-                libsForQt5.kalendar
-                libsForQt5.kate
-                libsForQt5.kdeconnect-kde
-                libsForQt5.kde-gtk-config
-                libsForQt5.plasma-framework
-                libsForQt5.plasma-nm
-                libsForQt5.plasma-pa
-                libsForQt5.sddm
+               libsForQt5.ark
+               libsForQt5.bismuth
+               libsForQt5.kalendar
+               libsForQt5.kate
+               libsForQt5.kdeconnect-kde
+               libsForQt5.kde-gtk-config
+               libsForQt5.neochat
+               libsForQt5.plasma-framework
+               libsForQt5.plasma-nm
+               libsForQt5.plasma-pa
+               libsForQt5.sddm
             ];
 }
