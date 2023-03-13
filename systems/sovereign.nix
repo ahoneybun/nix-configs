095f8d708d51298821777da3c4348a52b6d2d8a1
Aaron  Honeycutt <aaronhoneycutt@protonmail.com>
2023-03-13 13:25:29 +0000

    Update sovereign.nix

--- systems/sovereign.nix
+++ systems/sovereign.nix
@@ -1,3 +1,7 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
 { config, pkgs, ... }:
 
 {
@@ -5,6 +9,7 @@
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
 #      ./unstable.nix
+      ./stoners-space.nix
     ];
 
   boot.kernelPackages = pkgs.linuxPackages_latest;
@@ -53,26 +58,18 @@
       23.32.241.51 r3.o.lencr.org
     '';
 
-  fileSystems."/mnt/swapfile" =
-    { device = "/dev/disk/by-uuid/82672991-fe8a-485a-8dcf-7c8ae1282b6c";
-      fsType = "ext4";
-    };
-
-  services.hydra = {
-    enable = true;
-    hydraURL = "localhost:3000";
-    notificationSender = "hydra@localhost";
+#  services.hydra = {
+#    enable = true;
+#    hydraURL = "localhost:3000";
+#    notificationSender = "hydra@localhost";
     #buildMachinesFiles = [];
-    useSubstitutes = true;
-  };
+#    useSubstitutes = true;
+#  };
 
   security.acme.acceptTerms = true;
   security.acme.defaults.email = "aaronhoneycutt@proton.me";
   
   networking.hostName = "sovereign";
-  # Pick only one of the below networking options.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-  # networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
 
   # Set your time zone.
   # time.timeZone = "Europe/Amsterdam";
@@ -80,16 +77,6 @@
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
-
-  # Enable sound.
-  # sound.enable = true;
-  # hardware.pulseaudio.enable = true;
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.aaronh = {
@@ -104,14 +91,6 @@
     ];
   };
 
-  users.users.nathanielw = {
-    isNormalUser = true;
-    extraGroups = [ "wheel" "networkmanager" ];
-    packages = with pkgs; [
-      neofetch
-    ];
-  };
-
   users.users.builder = {
     isNormalUser = true;
     extraGroups = [ "wheel" ];
@@ -122,52 +101,34 @@
 
   environment.systemPackages = with pkgs; [
     acme-sh
+#    bundler
     git
     inetutils
+    jekyll
     mtr
     neofetch
+#    ruby_3_1
+    rubyPackages_3_1.jekyll
+#    rubyPackages_3_1.jekyll-feed
+#    rubyPackages_3_1.jekyll-redirect-from
+#    rubyPackages_3_1.webrick
+#    rubyPackages.webrick
     sysstat
     toybox
     tree
     wget
   ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
   # Enable the OpenSSH daemon.
   services.openssh = {
     enable = true;
-    permitRootLogin = "yes";
+  # permitRootLogin = "yes";
   };
 
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
   networking.usePredictableInterfaceNames = false;
   networking.useDHCP = false;
   networking.interfaces.eth0.useDHCP = true;
 
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  # system.copySystemConfiguration = true;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "22.11"; # Did you read the comment?
 
 }
