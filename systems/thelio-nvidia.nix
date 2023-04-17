5283872839845f390bc3a6b2494761c3bf8d0706
Aaron  Honeycutt <aaronhoneycutt@protonmail.com>
2023-04-17 17:15:39 +0000

    Add NVIDIA driver

--- systems/thelio-nvidia.nix
+++ systems/thelio-nvidia.nix
@@ -76,7 +76,15 @@
          neofetch
       ];
    };
-    
+   
+   # System76
+   hardware.system76.enableAll = true;
+
+   # NVIDIA
+   services.xserver.videoDrivers = [ "nvidia" ];   
+   hardware.opengl.enable = true;
+   hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.stable;
+
    # Allow Unfree
    nixpkgs.config.allowUnfree = true;
 
