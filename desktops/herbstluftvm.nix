2afd6ddd26550586482c056f421ef3b126c763cd
Aaron  Honeycutt <aaronhoneycutt@protonmail.com>
2023-04-25 20:34:43 +0000

    Update herbstluftvm.nix

--- desktops/herbstluftvm.nix
+++ desktops/herbstluftvm.nix
@@ -5,16 +5,14 @@
   services.xserver = {
     enable = true;
     displayManager.gdm.enable = true;
-    windowManager.herbstluftvm.enable = {
-      enable =true;
-      extraPackages = with pkgs; [
-        polybar # status bar
-        rofi # launcher
-     ];
-  }};
+    windowManager.herbstluftvm.enable = true;
+  };
 
   environment.systemPackages = (with pkgs; [
-    firefox
+    polybar # status bar
+    rofi # launcher
   ]);
 }
+
+
 
