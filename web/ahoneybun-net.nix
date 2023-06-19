4f0ab6050a5166a1ee5d2009542071c329b4c374
Aaron  Honeycutt <aaronhoneycutt@protonmail.com>
2023-06-19 16:42:04 +0000

    Update ahoneybun-net.nix

--- web/ahoneybun-net.nix
+++ web/ahoneybun-net.nix
@@ -1,20 +1,17 @@
 { config, pkgs, ... }:
 
 {
-  security.acme.acceptTerms = true;
-  security.acme.defaults.email = "aaronhoneycutt@proton.me";
-
-  services.nginx = {
-    enable = true;
-    virtualHosts = {
+    services.nginx = {
+      enable = true;
+      virtualHosts = {
       "ahoneybun.net" = {
       forceSSL = true;
       enableACME = true;
-      locations."/" = {
-        root = "/var/www";
-      };
-    };
-  };
-};
+       locations."/" = {
+         root = "/var/www/ahoneybun-net/_site";
+       };
+     };
+   };
+ };
 
 }
