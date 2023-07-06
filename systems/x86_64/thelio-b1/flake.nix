6b3f0da694dc2579bdd7e647efa37ccf929d488a
Aaron  Honeycutt <aaronhoneycutt@protonmail.com>
2023-07-06 17:25:05 +0000

    Update flake.nix

--- systems/x86_64/thelio-b1/flake.nix
+++ systems/x86_64/thelio-b1/flake.nix
@@ -16,88 +16,117 @@
           ./hardware-configuration.nix
 
           ({config, pkgs, ...}: {
-             nix = {
-                settings.auto-optimise-store = true;
-                settings.experimental-features = [ "nix-command" "flakes" ];
+            nix = {
+              settings.auto-optimise-store = true;
+              settings.experimental-features = [ "nix-command" "flakes" ];
            
-                gc = {
-                   automatic = true;
-                   dates = "weekly";
-                   options = "--delete-older-than 30d";
-                };
-             };
+              gc = {
+                automatic = true;
+                dates = "weekly";
+                options = "--delete-older-than 30d";
+              };
+            };
 
-             nixpkgs.config.allowUnfree = true; 
+            nixpkgs.config.allowUnfree = true; 
 
-             boot = {
-                # kernelPackages = pkgs.linuxPackages_latest;
-                kernelParams = [ "console=ttyS0,1920n8" ];
+            boot = {
+              # kernelPackages = pkgs.linuxPackages_latest;
+              kernelParams = [ "console=ttyS0,1920n8" ];
 
-                binfmt.emulatedSystems = [ "aarch64-linux" ];
+              binfmt.emulatedSystems = [ "aarch64-linux" ];
 
-                loader.systemd-boot.enable = true;
-                loader.systemd-boot.consoleMode = "0";
-             };
+              loader.systemd-boot.enable = true;
+              loader.systemd-boot.consoleMode = "0";
+            };
              
-             networking = {
-                hostName = "thelio-b1";
-                networkmanager.enable = true;
-             };
+            networking = {
+              hostName = "thelio-b1";
+              networkmanager.enable = true;
+            };
 
-             users.users.aaronh = {
-                isNormalUser = true;
-                extraGroups = [ "wheel" "networkmanager" ]; # Enable ‘sudo’ for the user.
-                packages = with pkgs; [
-                   cargo
-                   git
-                   git-lfs
-                   mdbook
-                   neofetch
-                   restic
-                   roboto-slab
+            users.users.aaronh = {
+              isNormalUser = true;
+              extraGroups = [ "wheel" "networkmanager" ]; # Enable ‘sudo’ for the user.
+              packages = with pkgs; [
+                cargo
+                git
+                git-lfs
+                mdbook
+                neofetch
+                restic
+                roboto-slab
 
-                   firefox
-                ];
-             };
+                firefox
+              ];
+            };
 
-             environment.systemPackages = with pkgs; [
-                avahi
-                dmidecode
-                libcamera
-                lshw
-                nix-index
-                sysstat
-                tree
-                unzip
-                wget
-             ];
+            environment.systemPackages = with pkgs; [
+              avahi
+              dmidecode
+              libcamera
+              lshw
+              nix-index
+              sysstat
+              tree
+              unzip
+              wget
+            ];
 
-             hardware.pulseaudio.enable = false;
+            # GNOME
+            services.xserver = {
+              enable = true;
+              displayManager.gdm.enable = true;
+              desktopManager.gnome.enable = true;
+            };
+
+            # Add GNOME packages
+            environment.systemPackages = (with pkgs; [
+              gnome.dconf-editor
+              gnome.gnome-tweaks
+              gnomeExtensions.appindicator
+              gnomeExtensions.pop-shell
+              gnomeExtensions.pop-launcher-super-key
+            ]);
+
+            # Remove GNOME packages
+            environment.gnome.excludePackages = (with pkgs; [
+              epiphany # web browser
+              gnome.geary
+              gnome.gnome-software
+              gnome-connections
+              gnome-photos
+              gnome-tour
+            ]);
+
+            # Services
+            services.udev.packages = with pkgs; [ gnome.gnome-settings-daemon ];
+
+            hardware.pulseaudio.enable = false;
              
-             security.rtkit.enable = true;
-             services.pipewire = {
-               enable = true;
-               alsa.enable = true;
-               alsa.support32Bit = true;
-               pulse.enable = true;
-             };
+            security.rtkit.enable = true;
+            services.pipewire = {
+              enable = true;
+              alsa.enable = true;
+              alsa.support32Bit = true;
+              pulse.enable = true;
+            };
 
-             services = {
-               fwupd.enable = true;
-               printing.enable = true;
-               openssh.enable = true;
-             };
+            services = {
+              fwupd.enable = true;
+              printing.enable = true;
+              openssh.enable = true;
+            };
  
-             services.avahi = {
-               enable = true;
-               nssmdns = true;
-               openFirewall = true;
-             };
+            services.avahi = {
+              enable = true;
+              nssmdns = true;
+              openFirewall = true;
+            };
   
-             system = {
-                stateVersion = "23.05";
-                autoUpgrade.enable = true;
-             };
+            system = {
+              stateVersion = "23.05";
+              autoUpgrade.enable = true;
+            };
           })
         ];
       };
