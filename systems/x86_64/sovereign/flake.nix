d1809f4e4cb012e8df6fae71aabf16757acd0a1e
Aaron  Honeycutt <aaronhoneycutt@protonmail.com>
2023-09-14 23:29:11 +0000

    Update flake.nix

--- systems/x86_64/sovereign/flake.nix
+++ systems/x86_64/sovereign/flake.nix
@@ -2,7 +2,7 @@
   description = "Sovereign";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/release-23.05";
   };
 
   outputs = { self, nixpkgs, ... }@inputs: {
