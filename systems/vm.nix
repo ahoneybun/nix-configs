0c3f382e019bd363dcf8ab851c20917f806ddbd0
Aaron  Honeycutt <aaronhoneycutt@protonmail.com>
2023-10-27 16:20:22 +0000

    Update vm.nix

--- systems/vm.nix
+++ systems/vm.nix
@@ -26,9 +26,6 @@
       };
    };
 
-   boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
-
-   nix.settings.extra-platforms = [ "aarch64-linux" ];
    nix.settings.auto-optimise-store = true;
    nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
