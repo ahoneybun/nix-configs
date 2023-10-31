6cc711c3ba75e497625f807f954aa3870096deb9
Aaron  Honeycutt <aaronhoneycutt@protonmail.com>
2023-10-31 23:29:29 +0000

    Add steam and 32bit support to shepard build

--- systems/x86_64/shepard/configuration.nix
+++ systems/x86_64/shepard/configuration.nix
@@ -4,4 +4,15 @@
     # Name your host machine
     networking.hostName = "shepard"; 
 
+    ## Enable 32 Bit libraries for applications like Steam
+    hardware.opengl.driSupport32Bit = true;
+
+    # Allow Unfree
+    nixpkgs.config.allowUnfree = true;
+
+    environment.systemPackages = 
+        with pkgs; 
+        [
+           steam
+        ]; 
 }
