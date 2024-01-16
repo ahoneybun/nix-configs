d329b8bc4084f9a3c1cc86ccaa8e383c17c48f1f
Aaron Honeycutt <aaronhoneycutt@protonmail.com>
2024-01-16 16:23:05 +0000

    Update home-manager version and swap Spotify for YouTube Music

--- home.nix
+++ home.nix
@@ -14,13 +14,13 @@
   # You can update Home Manager without changing this value. See
   # the Home Manager release notes for a list of state version
   # changes in each release.
-  home.stateVersion = "23.05";
+  home.stateVersion = "23.11";
 
   nixpkgs.config.allowUnfree = true; 
 
   home.packages = with pkgs; [
     # GUI
-    spotify
+    youtube-music
     vscode
 
     # CLI
