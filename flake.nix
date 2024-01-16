166a6d33bb4da55eca18673016de2eb2de845f68
Aaron Honeycutt <aaronhoneycutt@protonmail.com>
2024-01-16 17:53:29 +0000

    Fix brackets

--- flake.nix
+++ flake.nix
@@ -34,6 +34,7 @@
            }
           ./hardware-configuration.nix
          ];
+      };
 
       "dev-one" = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
@@ -54,6 +55,7 @@
            }
           ./hardware-configuration.nix
          ];
+      };
 
           ({config, pkgs, ...}: {
             nix = {
