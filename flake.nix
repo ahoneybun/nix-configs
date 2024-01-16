0512d22ca9a9dc4c4cac94536326751e94c7dff3
Aaron Honeycutt <aaronhoneycutt@protonmail.com>
2024-01-16 17:39:38 +0000

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -33,6 +33,27 @@
               home-manager.users.aaronh = import ./home.nix;
            }
           ./hardware-configuration.nix
+         ];
+
+      "dev-one" = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+           # Add Disko for disk management
+           disko.nixosModules.disko
+           ./disko-config.nix
+           {
+              _module.args.disks = [ "/dev/vda" ];
+           }
+           # Add Home-manager
+           home-manager.nixosModules.home-manager
+           {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+
+              home-manager.users.aaronh = import ./home.nix;
+           }
+          ./hardware-configuration.nix
+         ];
 
           ({config, pkgs, ...}: {
             nix = {
