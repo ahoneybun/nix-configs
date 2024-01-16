62d7e984dfd1b5bbc971b62b05f79c7a2182792f
Aaron Honeycutt <aaronhoneycutt@protonmail.com>
2024-01-16 16:32:12 +0000

    Disable VS Code due to unfree, the config isn't working. Move to unstable so 24.05 currently.

--- home.nix
+++ home.nix
@@ -14,14 +14,14 @@
   # You can update Home Manager without changing this value. See
   # the Home Manager release notes for a list of state version
   # changes in each release.
-  home.stateVersion = "23.11";
+  home.stateVersion = "24.05";
 
   nixpkgs.config.allowUnfree = true; 
 
   home.packages = with pkgs; [
     # GUI
     #youtube-music
-    vscode
+    #vscode
 
     # CLI
     mdbook
