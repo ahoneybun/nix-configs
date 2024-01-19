74cc1eac4ed507f76371ab4f38b977122aba78cc
Aaron Honeycutt <aaronhoneycutt@protonmail.com>
2024-01-19 13:37:37 +0000

    Add thelio-b1

--- flake.nix
+++ flake.nix
@@ -37,6 +37,19 @@
                ./hardware-configuration.nix
             ];
          };
+         
+         "thelio-b1" = nixpkgs.lib.nixosSystem {
+            system = "x86_64-linux";
+            modules = [
+               # Add Disko for disk management
+               disko.nixosModules.disko
+               ./disko-config.nix
+               ./gnome.nix
+               ./thelio-nvidia.nix
+               ./configuration.nix
+               ./hardware-configuration.nix
+            ];
+         };
 
          "vm" = nixpkgs.lib.nixosSystem {
             system = "x86_64-linux";
