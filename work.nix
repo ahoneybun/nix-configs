8bd8fd2344701173fa796f185c08a53a386f4abc
Aaron Honeycutt <aaronhoneycutt@protonmail.com>
2024-01-30 13:08:32 -0700

    add: add work nix file for work packages

---
+++ work.nix
@@ -0,0 +1,22 @@
+{ config, pkgs, ... }: 
+
+{
+
+   # virt-manager
+   virtualisation.libvirtd.enable = true;
+
+   # Packages
+   environment.systemPackages = with pkgs; [
+      # Comms
+         slack
+
+      # Web
+         google-chrome
+
+      # VM
+      quickemu
+      spice
+      spice-gtk
+      virt-manager
+   ];
+}
