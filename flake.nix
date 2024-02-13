c332c8bcb3e2ee1c5df562c5aa0cfa4a1053b9f7
Aaron Honeycutt <aaronhoneycutt@protonmail.com>
2024-02-13 06:50:07 -0700

    flake: remove work systems and HP OMEN

--- flake.nix
+++ flake.nix
@@ -27,43 +27,6 @@
             ];
          };
 
-         "dev-one" = nixpkgs.lib.nixosSystem {
-            system = "x86_64-linux";
-            modules = [
-               # Add Disko for disk management
-               disko.nixosModules.disko
-               ./disko-config.nix
-               ./gnome.nix
-               ./configuration.nix
-               ./hardware-configuration.nix
-               home-manager.nixosModules.home-manager
-               {
-                  home-manager.useGlobalPkgs = true;
-                  home-manager.useUserPackages = true;
-                  home-manager.users.aaronh = import ./home.nix;
-               }
-            ];
-         };
-
-         "omen" = nixpkgs.lib.nixosSystem {
-            system = "x86_64-linux";
-            modules = [
-               # Add Disko for disk management
-               disko.nixosModules.disko
-               ./disko-config.nix
-               ./gnome.nix
-               ./hp-omen.nix
-               ./configuration.nix
-               ./hardware-configuration.nix
-               home-manager.nixosModules.home-manager
-               {
-                  home-manager.useGlobalPkgs = true;
-                  home-manager.useUserPackages = true;
-                  home-manager.users.aaronh = import ./home.nix;
-               }
-            ];
-         };
-         
          "garrus" = nixpkgs.lib.nixosSystem {
             system = "x86_64-linux";
             modules = [
@@ -100,25 +63,6 @@
                   home-manager.users.aaronh = import ./home.nix;
                }
                nixos-hardware.nixosModules.pine64-pinebook-pro
-            ];
-         };
-
-         "thelio-b1" = nixpkgs.lib.nixosSystem {
-            system = "x86_64-linux";
-            modules = [
-               # Add Disko for disk management
-               disko.nixosModules.disko
-               ./disko-config.nix
-               ./gnome.nix
-               ./thelio-nvidia.nix
-               ./configuration.nix
-               ./hardware-configuration.nix
-               home-manager.nixosModules.home-manager
-               {
-                  home-manager.useGlobalPkgs = true;
-                  home-manager.useUserPackages = true;
-                  home-manager.users.aaronh = import ./home.nix;
-               }
             ];
          };
 
