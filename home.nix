a277ea4f54a5fbfd4022cd6f2d700cf42c7a4215
Aaron Honeycutt <aaronhoneycutt@protonmail.com>
2024-02-15 17:02:51 -0700

    hm: change 24.05 to 23.11

--- home.nix
+++ home.nix
@@ -14,7 +14,7 @@
   # You can update Home Manager without changing this value. See
   # the Home Manager release notes for a list of state version
   # changes in each release.
-  home.stateVersion = "24.05";
+  home.stateVersion = "23.11";
 
   nixpkgs.config = {
     allowUnfree = true;
