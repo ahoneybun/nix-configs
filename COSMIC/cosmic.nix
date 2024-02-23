7de7116145600f815dfa1c79060126cde8bb0294
Aaron Honeycutt <aaronhoneycutt@protonmail.com>
2024-02-23 18:18:22 +0000

    cosmic: remove deps

--- COSMIC/cosmic.nix
+++ COSMIC/cosmic.nix
@@ -12,21 +12,5 @@
   environment.systemPackages = (with pkgs; [
      rustc
      cargo
-     cosmic-launcher
-     cosmic-applibrary
-     cosmic-panel
-     cosmic-greeter
-     cosmic-settings
-     cosmic-settings-daemon
-     cosmic-screenshot
-     cosmic-term
-     cosmic-files
-     cosmic-edit
-     xdg-desktop-portal-cosmic
-     # For screenshots until cosmic-screenshot is updated
-     ksnip
   ]);
-
-  # Fix cosmic-greeter
-  security.pam.services.cosmic-greeter = {};
 }
