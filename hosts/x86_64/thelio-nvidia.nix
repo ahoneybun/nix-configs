3fe6d2f61433766419ddc5d7f4f558dd9f33b7ce
Aaron Honeycutt <aaronhoneycutt@protonmail.com>
2024-02-27 13:17:00 +0000

    thelio-nvidia: add modesetting

--- hosts/x86_64/thelio-nvidia.nix
+++ hosts/x86_64/thelio-nvidia.nix
@@ -11,6 +11,7 @@
     services.xserver.videoDrivers = [ "nvidia" ];   
     hardware.opengl.enable = true;
     hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.stable;
+    hardware.nvidia.modesetting.enable = true;
 
     # Allow Unfree
     nixpkgs.config.allowUnfree = true;
