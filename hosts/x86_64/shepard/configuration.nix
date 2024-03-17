73653f17220c2221d3354d58bfd97d7776d0f5d6
Aaron Honeycutt <aaronhoneycutt@protonmail.com>
2024-03-17 23:59:23 +0000

    add aarch64-linux support on shepard

--- hosts/x86_64/shepard/configuration.nix
+++ hosts/x86_64/shepard/configuration.nix
@@ -9,6 +9,9 @@
         driSupport32Bit = true;
     };
 
+    # Enable binfmt emulation of aarch64-linux.
+    boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
+
     # Allow Unfree
     nixpkgs.config.allowUnfree = true;
 
