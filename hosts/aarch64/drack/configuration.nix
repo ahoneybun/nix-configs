fdd87f3873653312c7251a1322faa79e96bee5bc
Aaron Honeycutt <aaronhoneycutt@protonmail.com>
2024-05-01 23:12:28 +0000

    Add kernel modules for drack (X13)

--- hosts/aarch64/drack/configuration.nix
+++ hosts/aarch64/drack/configuration.nix
@@ -13,6 +13,41 @@
     "arm64.nopauth"
   ];
 
+  boot.kernelModules = [
+    # Core
+    "qnoc-sc8280xp"
+
+    # NVME
+    "phy_qcom_qmp_pcie"
+    "pcie_qcom"
+    "nvme"
+
+    # Keyboard
+    "i2c_qcom_geni"
+    "i2c_hid_of"
+    "hid_generic"
+
+    # Display
+    "pwm_bl"
+    "qrtr"
+    "phy_qcom_edp"
+    "i2c_qcom_geni"
+    "gpio_sbu_mux"
+    "pmic_glink_altmode"
+    "spmi_pmic_arb"
+    "phy_qcom_qmp_combo"
+    "qcom_spmi_pmic"
+    "pinctrl_spmi_gpio"
+    "leds_qcom_lpg"
+    "panel_edp"
+    "msm"
+
+    # USB (required for installation from USB)
+    "qcom_q6v5_pas"  # This module loads a lot of FW blobs
+    "usb_storage"
+    "uas"
+    ];
+
   networking.hostName = "drack";
 
   services.udev.extraRules = ''
