092d15be68c07337dd23c4dd09e5a40e110fb11b
Aaron Honeycutt <aaronhoneycutt@protonmail.com>
2024-05-01 23:07:55 +0000

    Add udev rules for drack (X13s)

--- hosts/aarch64/drack/configuration.nix
+++ hosts/aarch64/drack/configuration.nix
@@ -15,4 +15,11 @@
 
   networking.hostName = "drack";
 
+  services.udev.extraRules = ''
+    # Enable touchscreen on X13s
+    ACTION=="add", SUBSYSTEM=="drivers", KERNEL=="i2c_hid_of", ATTR{bind}="4-0010"
+    # Set wifi MAC on X13s
+    SUBSYSTEM=="net", ACTION=="add", ATTRS{vendor}=="0x17cb", ATTRS{device}=="0x1103", PROGRAM="/usr/share/ubuntu-x13s-settings/set-wifi-mac-addr %k"
+  '';
+
 }
