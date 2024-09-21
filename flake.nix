38c1e1fecfc1939d53b0d7086df79d23f2e7618c
Aaron Honeycutt <aaronhoneycutt@protonmail.com>
2024-09-21 01:06:28 +0000

    Add new file

---
+++ flake.nix
@@ -0,0 +1,104 @@
+{
+   description = "Generic System Flake file";
+
+   inputs = {
+      nixpkgs.url = "github:NixOS/nixpkgs/release-24.05";
+      # nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+      disko = {
+         url = github:nix-community/disko;
+         inputs.nixpkgs.follows = "nixpkgs";
+      };
+      nix-software-center.url = "github:snowfallorg/nix-software-center";
+      home-manager = {
+         url = "github:nix-community/home-manager";
+         inputs.nixpkgs.follows = "nixpkgs";
+      };
+      nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+   };
+
+   outputs = { self, nixpkgs, disko, home-manager, nixos-hardware, ... }@inputs: {
+      nixosConfigurations = {
+         "nixos" = nixpkgs.lib.nixosSystem {
+            system = "x86_64-linux";
+            modules = [
+               # Add Disko for disk management
+               disko.nixosModules.disko
+               ./disko-config.nix
+               ./configuration.nix
+               ./hardware-configuration.nix
+            ];
+         };
+
+         "shepard" = nixpkgs.lib.nixosSystem {
+            system = "x86_64-linux";
+            modules = [
+               # Add Disko for disk management
+               disko.nixosModules.disko
+               ./disko-config.nix
+               ./gnome.nix
+               ./shepard.nix
+               ./configuration.nix
+               ./hardware-configuration.nix
+               home-manager.nixosModules.home-manager
+               {
+                  home-manager.useGlobalPkgs = true;
+                  home-manager.useUserPackages = true;
+                  home-manager.users.aaronh = import ./home.nix;
+               }
+            ];
+         };
+
+         "garrus" = nixpkgs.lib.nixosSystem {
+            system = "x86_64-linux";
+            modules = [
+               # Add Disko for disk management
+               disko.nixosModules.disko
+               ./disko-config.nix
+               ./gnome.nix
+               ./garrus.nix
+               ./configuration.nix
+               ./hardware-configuration.nix
+               home-manager.nixosModules.home-manager
+               {
+                  home-manager.useGlobalPkgs = true;
+                  home-manager.useUserPackages = true;
+                  home-manager.users.aaronh = import ./home.nix;
+               }
+            ];
+         };
+
+         "jaal" = nixpkgs.lib.nixosSystem {
+            system = "aarch64-linux";
+            modules = [
+               # Add Disko for disk management
+               disko.nixosModules.disko
+               ./disko-config.nix
+               ./gnome.nix
+               ./jaal.nix
+               ./configuration.nix
+               ./hardware-configuration.nix
+               home-manager.nixosModules.home-manager
+               {
+                  home-manager.useGlobalPkgs = true;
+                  home-manager.useUserPackages = true;
+                  home-manager.users.aaronh = import ./home.nix;
+               }
+               nixos-hardware.nixosModules.pine64-pinebook-pro
+            ];
+         };
+
+         "vm" = nixpkgs.lib.nixosSystem {
+            system = "x86_64-linux";
+            modules = [
+               # Add Disko for disk management
+               disko.nixosModules.disko
+               ./disko-config.nix
+               ./vm.nix
+               ./configuration.nix
+               ./hardware-configuration.nix
+            ];
+         };
+
+      };
+   };
+}
